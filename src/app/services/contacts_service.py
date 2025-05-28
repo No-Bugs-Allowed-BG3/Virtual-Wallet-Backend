13b3a848c9ec9c36c75a966e38fdccfd44acfb96
eimihaylov <emil.mihaylov.a69@learn.telerikacademy.com>
2025-05-28 19:08:12 +0300

    Implemented create contact by username, phone or email

--- src/app/services/contacts_service.py
+++ src/app/services/contacts_service.py
@@ -9,12 +9,28 @@ from app.persistence.contacts.contact import Contact
 from app.schemas.contact import ContactResponse
 from app.services.users_service import _get_user_id_by_username, _get_user_id_by_email, _get_user_id_by_phone
 
+async def _find_user_id(
+    db: AsyncSession,
+    username: str | None,
+    phone: str | None,
+    email: str | None
+) -> UUID | None:
+    if username:
+        return await _get_user_id_by_username(db, username)
+    if phone:
+        return await _get_user_id_by_phone(db, phone)
+    if email:
+        return await _get_user_id_by_email(db, email)
+    return None
+
 async def create_contact(
     db: AsyncSession,
     user_id: UUID,
-    username: str
+    username: str | None = None,
+    phone: str | None = None,
+    email: str | None = None
 ) -> ContactResponse:
-    contact_id = _get_user_id_by_username(db, username)
+    contact_id = _find_user_id(db, username, phone, email)
     if not contact_id:
         raise USER_NOT_FOUND
     contact = Contact(user_id=user_id,contact_id=contact_id)
